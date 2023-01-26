6455c932b68f90e8edaaa9f74e62c9eff7d974ca
Komoliddin <komoliddinrahmatullayevo7@gmail.com>
2023-01-26 10:02:17 +0500

    fuuls commit

---
+++ main.js
@@ -0,0 +1,9 @@
+/* header start */
+let navMenu = document.querySelector(".nav__menu")
+let navList = document.querySelector(".nav__list1")
+
+navMenu.addEventListener("click", () => {
+    navList.classList.toggle("active")
+})
+
+/* header end */
